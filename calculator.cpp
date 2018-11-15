7d3425beeecfd27acba6772126ffe8fd0169e6ba
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    Enter method Div, Pow and patches

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,19 @@ int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+int Calculator::Div (double a, double b)
+{
+    return a / b;
+}
+int Calculator::Pow (double a, double b)
+{
+    for (int i = 0; i < b; i++){
+        a = a * a;
+    }
+    return a;
+}
--- calculator.h
+++ calculator.h
@@ -4,8 +4,12 @@
 class Calculator
 {
     public:
+
         int Add (double, double);
         int Sub (double, double);
+        int Mul (double, double);
+        int Div (double, double);
+        int Pow (double, double);
 };
 
 #endif//CALCULATOR_H
